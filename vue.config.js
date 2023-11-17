abcf91b764dafff8d94a338367a66d5281cce10d
황인찬 <klklgooa@gmail.com>
2023-11-17 23:30:34 +0900

    11.17

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   outputDir: './docs',
-  publicPath: 'https://klklgooa.github.io/MyPortfolio_vue/',
+  publicPath: '/MyPortfolio_vue/',
   transpileDependencies: true,
   lintOnSave:false
 })
